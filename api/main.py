c7b7d0f3dea6124c6c8f7bfc6b4d4acb6d873c71
Yossapol Witayanont <reef_3llen@hotmail.com>
2025-07-15 17:05:29 +0700

    feat: :zap: warm up llm server

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from api import rag
-from api.env import FILES_PATH
+from api.env import FILES_PATH, MODEL_NAME, OLLAMA_API
 from api.watcher import start_watcher
+import requests
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 watcher = None
@@ -9,6 +11,7 @@ watcher = None
 @app.on_event("startup")
 def startup_event():
     global watcher
+    warm_up_ollama()
     rag.build_index()
     watcher = start_watcher(FILES_PATH)
 
@@ -17,6 +20,22 @@ def shutdown_event():
     if watcher:
         watcher.stop()
         watcher.join()
+
+
+def warm_up_ollama():
+    try:
+        print("Warming up LLM...")
+        requests.post(
+            f"{OLLAMA_API}/api/generate",
+            json={
+                "model": MODEL_NAME, 
+                "prompt": "Hello", 
+                "stream": False
+            },
+            timeout=60
+        )
+    except Exception as e:
+        print(f"Warmup failed: {e}")
 
 @app.post("/ask")
 async def ask(request: Request):
